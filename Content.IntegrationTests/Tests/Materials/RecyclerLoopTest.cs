a4ea004111009c63782a939498a177641ddf8fc8
Bea Shakow <beashakow@gmail.com>
2026-03-15 14:47:30 -0400

    un-parameterized Recyclables

--- Content.IntegrationTests/Tests/Materials/RecyclerLoopTest.cs
+++ Content.IntegrationTests/Tests/Materials/RecyclerLoopTest.cs
@@ -1,9 +1,11 @@
 using Content.IntegrationTests.Tests.Interaction;
 using Content.Server.Materials;
 using Content.Shared.Materials;
-
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Dictionary;
 using Content.IntegrationTests.Utility;
 using Robust.Shared.Prototypes;
+using NetCord;
+using System.Linq;
 
 namespace Content.IntegrationTests.Tests.Materials;
 //ProtoIDs we may need
@@ -16,15 +18,39 @@ namespace Content.IntegrationTests.Tests.Materials;
 [TestOf(typeof(MaterialReclaimerComponent))]
 public sealed class RecyclingLoopTest : InteractionTest
 {
-    private static string[] _recyleables = GameDataScrounger.EntitiesWithComponent("PhysicalCompositionComponent");
-    private static string[] _reclaimers = GameDataScrounger.EntitiesWithComponent("MaterialReclaimerComponent");
+    private static string[] _materials = GameDataScrounger.EntitiesWithComponent("PhysicalComposition");
+    private static string[] _reclaimers = GameDataScrounger.EntitiesWithComponent("MaterialReclaimer");
+
+
 
     /// <summary>
     /// For each entity that recycles into materials, recycle it and check that
     /// </summary>
-    [Test]
-    public async Task RecyclerLoopTest([ValueSource(nameof(_recyleables))] string reclaimerID, [ValueSource(nameof(_recyleables))] string recyclableID)
+    [Test, Combinatorial]
+    [TestOf(typeof(MaterialReclaimerSystem))]
+    [TestOf(typeof(MaterialReclaimerComponent))]
+    public async Task RecyclerLoopTest(string reclaimerID)
     {
-        //yippee
+        var materialReclaimerSystem = SEntMan.System<MaterialReclaimerSystem>();
+
+        //go through all recyclable items, compile a list of produceable materials
+        string[] produceableMaterials = [];
+        foreach (String itemID in _materials)
+        {
+            EntityPrototype item = ProtoMan.Index(itemID);
+            item.TryGetComponent<PhysicalCompositionComponent>("PhysicalCompositionComponent", out var compositionComp);
+            foreach ((var mat, var value) in compositionComp.MaterialComposition) //for each material they spawn
+            {
+                //If its not already in producedMaterials, add it
+                if (!produceableMaterials.Contains(mat))
+                {
+                    produceableMaterials.Append(mat);
+                }
+            }
+        }
+
+        //For each produceable Material, assert that it is not recyclable
+
+
     }
 }
